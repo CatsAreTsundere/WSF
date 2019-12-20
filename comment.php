98d527e2a884ad818afd7ead60e8206c980a6752
CatsAreTsundere <54909010+CatsAreTsundere@users.noreply.github.com>
2019-12-20 11:24:17 -0500

    Clean up
    comment.php: Added indentation to code and a few comments
    index.php: Indentation and comments added.
    Deleted a form factor I found redundant: asking what rpg game reader has played when the  comment form asks already what games you've already played.

--- comment.php
+++ comment.php
@@ -1,64 +1,67 @@
 <!doctype php>
 <html>
 
-<head>
-<title>Comments</title>
-</head>
+    <head>
+         <title>Comments</title>
+    </head>
 <body bgcolor="c9e9f6">
-<h1>Here are some of your comments</h1>
+    
+    <h1>Here are some of your comments</h1>
 
 <?php
-$user = htmlspecialchars($_GET['user']);
-$game = htmlspecialchars($_GET['game']);
-$cmmt = htmlspecialchars($_GET['message']);
-
-$nuser = filter_var($user, FILTER_SANITIZE_STRING);
-$ngame = filter_var($game, FILTER_SANITIZE_STRING);
-$ncmmt = filter_var($cmmt, FILTER_SANITIZE_STRING);
-
-
-
+    $user = htmlspecialchars($_GET['user']);
+    $game = htmlspecialchars($_GET['game']);
+    $cmmt = htmlspecialchars($_GET['message']);
+    //Input sanitation, turns stuff like ' or * othr variables that dont messs with PHP
+    $nuser = filter_var($user, FILTER_SANITIZE_STRING);
+    $ngame = filter_var($game, FILTER_SANITIZE_STRING);
+    $ncmmt = filter_var($cmmt, FILTER_SANITIZE_STRING);
 ?>
 
 <div>
 
 <?php
-//db log in info
-$servername = "localhost";
-$username = "catsu";
-$password = "Elisanne009";
-$dbname = "commtbl";
+//db login info
+    $servername = "localhost";
+    $username = "catsu";
+    $password = "Elisanne009";
+    $dbname = "commtbl";
 
 
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
 //connection check
-if(!$conn){
-    die("Connection failed: " . mysqli_connect_error());
-}
+    if(!$conn){
+        die("Connection failed: " . mysqli_connect_error());
+    }
 
 
-$sql = "SELECT userid, name, game, Comment from comm;";
-$sql_ins ="INSERT INTO comm (name, game, Comment) values ('{$nuser}','{$ngame}','{$ncmmt}');";
+    $sql = "SELECT userid, name, game, Comment from comm;";
+    $sql_ins ="INSERT INTO comm (name, game, Comment) values ('{$nuser}','{$ngame}','{$ncmmt}');";
 
+    //Inseting data into the the database
+    if (mysqli_query($conn, $sql_ins)){
+        echo "Thank you for your comment! \r\n";
+        echo "<br>";
+    }
 
-if (mysqli_query($conn, $sql_ins)){
-    echo "Thank you for your comment! \r\n";
-    echo "<br>";
-}
-else {
-    Echo "That didn't go through.. Error: " . $sql . mysqli_error($conn);
-}
-$result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result)>0){
-    //otp on screen
+    else {
+        Echo "That didn't go through.. Error: " . $sql . mysqli_error($conn);
+    }
+    
+    //Checking results of inseting stuff
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result)>0){
+    //output on screen
     while ($row = mysqli_fetch_assoc($result)){
         echo "\r\nID:" . $row["userid"] . " - Name: " .  $row["name"]. "- Game: " . $row["game"] . "- Message: " . $row["Comment"]. "<br>";
     }
+    //If no output put (basically checking if SQL gets the inputs to the database)
     }else {
         echo "0 results";
     }
-
+//Close connection
 mysqli_close($conn);
 ?>
 
--- index.php
+++ index.php
@@ -1,96 +1,83 @@
 <!doctype php>
+
 <html>
-
-
-
-<head>
-    <script src="empty.js"></script>
-
-    <title>Granblue Fantasy</title>
-</head>
+    <head>
+        <!--Source location for the userside input verification-->
+        <script src="empty.js"></script>
+        <!--Title of page-->
+        <title>Granblue Fantasy</title>
+    </head>
+<!--Changing background colour of the HTML page-->
 <body bgcolor="c9e9f6">
-<h1>A page about a browser game I really like</h1>
+    <h1>A page about a browser game I really like</h1>
 
-<p>Choose what gender you indentify the most with: 
-    <form>
-        <select id="gender" onchange="charaname()"> <!--Choosing gender to add some text into the html-->
-                <option selected>Select</option>
-                <option value="Gran">Male</option> 
-                <option value="Djeeta">Female</option>
-        </select>
-        
+    <p>Choose what gender you indentify the most with: 
+        <form>
+            <select id="gender" onchange="charaname()"> <!--Choosing gender to add some text into the html-->
+                    <option selected>Select</option>
+                    <option value="Gran">Male</option> 
+                    <option value="Djeeta">Female</option>
+            </select>    
+        </form> 
+    </p>
+
+    <p><a href="https://en.wikipedia.org/wiki/Granblue_Fantasy">GranblueFantasy</a> is a really fun game that is heavily inspired by Square Enix</p>
+    <p>I really enjoy the story, it revolves around the sky. There are continent islands that are floating over clouds and no one knows what lies beneath them.</p>
+
+    <!-- simple script to change name dsrc="empty.js"epending on the selected gender -->
+    <script> 
+        function charaname() {
+            var x = document.getElementById("gender").value;
+            document.getElementById("named").innerHTML = "Our main character is named " + x + ". You take charge of the decisions the main character does (although everything is scripted and the decisions don't matter at all)."; //Changes name depening on the gender chosen by 
+            } 
+    </script>
+
+    <p>You live on the Port Breeze Archipelago. And you live a fairly normal life.</p>
+    <p id="named"></p><p>You are followed a little dragon called Vyrn (be careful not to call him a lizard). He's been your companion for as long as you can remember. You don't have any family living with you and you're pretty independant.</p>
+    <p>All you have left to remember your family is a map of the skydom that was left behind by your father. He was a great skyfarer(a sort of adventurer) as far as you can remember.</p>
+
+    <img src="images/vyrnapple.gif "> <!--Image brough up through a sub directory-->
+    
+    <p>Do you think hes a dragon or a lizard?</p>
+    <!--Simple alerts on click-->
+    <button type="button" onclick="alert('HE IS NOT A LIZARD!!!')">Lizard</button><button type="button" onclick="alert('He is most definitely a fully fledge dragon')">Dragon</button>
+
+
+    <p><img src="images/LyriaKatalina.png" width="240" height="200" style="float: left" title="Lyria(left) being protected by Katalina(right)">  Another very important sidekick is Lyria. A sky blue haired girl. She wears a white dress and has a gold necklace with big gem in the middle. You meet her for the first time running away from Imperial Knights. You try and save her, but after accident (and summoning Bahamut to help you in your fight) you end up Life Linked to her. Meaning your that if either of you die, the other one dies with them. </p>
+    <p> You also meet Katalina, Lyria's guard and older sister figure. She explains to you that Lyria was an experiment created to summon "Primal Beings" such as Bahamut to be used during war.</p>
+    <p>As you play, there will be many characters joining your adventures. Either by story or by <a href="https://en.wikipedia.org/wiki/Gacha_game">gacha</a> means(basicly gambling for characters with ingame currency)</p>
+    <p>Characters range from big buff cow horned dudes to small elf eared beings. Male or female. Even some Primals that Lyria could summon.</p>
+    <p>You'll encounter many primal beings you'll need to fight. Many people you need to help.</p>
+
+    <p>What rpg games do you have experience in?</p>
+
+
+    <p>No matter what the rpg is, you migh find a similar </p>
+    <p>The gameplay is like a standard turnbased RPG ala Final Fantasy. You have 4 Fighters (one slot reserved for the main character). Each characters have a set of skill that allow them to do their assigned roll.</p>
+    <p>The game functions on raids. You can have up to 30 teams. Each dealing damage to the single to up to 3 enemies. Team up with other to beat high level </p>
+    <p>Like any other mobile game, there are tones of eventsthat have their own stories and raids. In my opinion, the best events are the What Makes the Sky Blue event. The story is great. It explained the story of Sandalphon and the other Primarchs (high level angels) in this universe. <strike>Although it was suposed to be a Lyria event.</strike></p>
+    <p>The WMTSB trilogy explained alot about character we got in the gacha pool but knew nothing about. And the Primarch we fought. But never got an explaination as to why.</p>
+
+    <p>How was my real quick explanation of the game?</p>
+
+    <!-- form factor for adding a comment -->
+    <!-- action /comment.php to add entries to database -->
+    <!-- also on submit runs checkEmpty() function found in a different .js file -->
+    <form name="commenting" onsubmit="return checkEmpty();" action="/comment.php" method="get">
+        <br>Username:</br>
+            <textarea name="user" rows="1" cols="10"></textarea>
+        <br>Games you've played:</br>
+            <textarea name="game"   rows="2" cols="30"></textarea>
+        <br>Comment:</br>
+            <textarea name="message" rows="5" cols="30"></textarea>    
+        <br>
+        <input type="submit" name="submit" value="Submit">
     </form>
-</p>
-<p><a href="https://en.wikipedia.org/wiki/Granblue_Fantasy">GranblueFantasy</a> is a really fun game that is heavily inspired by Square Enix</p>
-<p>I really enjoy the story, it revolves around the sky. There are continent islands that are floating over clouds and no one knows what lies beneath them.</p>
-<!-- simple script to change name dsrc="empty.js"epending on the selected gender -->
-<script> 
-    function charaname() {
-        var x = document.getElementById("gender").value;
-        document.getElementById("named").innerHTML = "Our main character is named " + x + ". You take charge of the decisions the main character does (although everything is scripted and the decisions don't matter at all)."; //Changes name depening on the gender chosen by 
-        } 
-</script>
 
-<p>You live on the Port Breeze Archipelago. And you live a fairly normal life.</p>
-<p id="named"></p><p>You are followed a little dragon called Vyrn (be careful not to call him a lizard). He's been your companion for as long as you can remember. You don't have any family living with you and you're pretty independant.</p>
-<p>All you have left to remember your family is a map of the skydom that was left behind by your father. He was a great skyfarer(a sort of adventurer) as far as you can remember.</p>
-<img src="images/vyrnapple.gif "> <!--Image brough up through a sub directory-->
-<p>Do you think hes a dragon or a lizard?</p>
-
-<button type="button" onclick="alert('HE IS NOT A LIZARD!!!')">Lizard</button><button type="button" onclick="alert('He is most definitely a fully fledge dragon')">Dragon</button>
-
-
-
-<!--<script>
-    function vyrnpic(){
-    var v = document.getElementById("lod").value;
-    if( v === "lizard" )
-    {
-        document.getElementById("vyrn").innerHTML = "He is not a lizard."
-    }
-    else if ( v === "dragon" )
-    {src="empty.js"
-        document.getElementById("vyrn").innerHTML = "He is indeed a dragon"
-    }
-    }
-</script>-->
-<p id="Vyrn"></p>
-<p><img src="images/LyriaKatalina.png" width="240" height="200" style="float: left" title="Lyria(left) being protected by Katalina(right)">  Another very important sidekick is Lyria. A sky blue haired girl. She wears a white dress and has a gold necklace with big gem in the middle. You meet her for the first time running away from Imperial Knights. You try and save her, but after accident (and summoning Bahamut to help you in your fight) you end up Life Linked to her. Meaning your that if either of you die, the other one dies with them. </p>
-<p> You also meet Katalina, Lyria's guard and older sister figure. She explains to you that Lyria was an experiment created to summon "Primal Beings" such as Bahamut to be used during war.</p>
-<p>As you play, there will be many characters joining your adventures. Either by story or by <a href="https://en.wikipedia.org/wiki/Gacha_game">gacha</a> means(basicly gambling for characters with ingame currency)</p>
-<p>Characters range from big buff cow horned dudes to small elf eared beings. Male or female. Even some Primals that Lyria could summon.</p>
-<p>You'll encounter many primal beings you'll need to fight. Many people you need to help.</p>
-
-<p>What rpg games do you have experience in?</p>
-<form action="/gn.php">
-    <input name="gamename" type="text">
-    <br><br>
-    <input type="submit">
-</form>
-
-<p>No matter what the rpg is, you migh find a similar </p>
-<p>The gameplay is like a standard turnbased RPG ala Final Fantasy. You have 4 Fighters (one slot reserved for the main character). Each characters have a set of skill that allow them to do their assigned roll.</p>
-<p>The game functions on raids. You can have up to 30 teams. Each dealing damage to the single to up to 3 enemies. Team up with other to beat high level </p>
-<p>Like any other mobile game, there are tones of eventsthat have their own stories and raids. In my opinion, the best events are the What Makes the Sky Blue event. The story is great. It explained the story of Sandalphon and the other Primarchs (high level angels) in this universe. <strike>Although it was suposed to be a Lyria event.</strike></p>
-<p>The WMTSB trilogy explained alot about character we got in the gacha pool but knew nothing about. And the Primarch we fought. But never got an explaination as to why.</p>
-
-<p>How was my real quick explanation of the game?</p>
-
-<!-- form factor for adding a comment -->
-<form name="commenting" onsubmit="return checkEmpty();" action="/comment.php" method="get">
-    <br>Username:</br>
-    <textarea name="user" rows="1" cols="10"></textarea>
-    <br>Games you've played:</br>
-    <textarea name="game"   rows="2" cols="30"></textarea>
-    <br>Comment:</br>
-    <textarea name="message" rows="5" cols="30"></textarea>    
-    <br>
-    <input type="submit" name="submit" value="Submit">
-</form>
-
-<h2>
-    Thanks for reading! <img src="images/LyriaRead.gif" width="100" height="100">
-</h2>
-<p>If you want to see my other hobby click <a href="site2.html">here</a>!</p>
-</body>
+    <h2>
+        Thanks for reading! <img src="images/LyriaRead.gif" width="100" height="100">
+    </h2>
+    <!--Link to second site-->
+    <p>If you want to see my other hobby click <a href="site2.html">here</a>!</p>
+    </body>
 </html>
